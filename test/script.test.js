cefbdb77c6b2e0e7abef1763a73696d02ac306c1
1pav <60271007+1pav@users.noreply.github.com>
2021-02-23 17:16:05 +0100

    Fix number format whith small negative numbers

--- test/script.test.js
+++ test/script.test.js
@@ -20,3 +20,30 @@ test('Negative profit', () => {
     expect(buyOrderFee).toBeCloseTo(0.14);
     expect(sellOrderFee).toBeCloseTo(0.14);
 });
+
+test('Format positive numbers', () => {
+    expect(target.formatNumber(123.454, "eur")).toBe("€123.45");
+    expect(target.formatNumber(123.456, "usd")).toBe("$123.46");
+    expect(target.formatNumber(1234.567, "eur")).toBe("€1,234.57");
+    expect(target.formatNumber(1234, "usd")).toBe("$1,234.00");
+    expect(target.formatNumber(123, "eur")).toBe("€123.00");
+    expect(target.formatNumber(12.30, "usd")).toBe("$12.30");
+    expect(target.formatNumber(12.3, "eur")).toBe("€12.30");
+    expect(target.formatNumber(0.005, "usd")).toBe("$0.01");
+});
+
+test('Format negative numbers', () => {
+    expect(target.formatNumber(-0.01, "eur")).toBe("-€0.01");
+    expect(target.formatNumber(-0.009, "usd")).toBe("-$0.01");
+    expect(target.formatNumber(-0.005, "eur")).toBe("-€0.01");
+});
+
+test('Format zero', () => {
+    expect(target.formatNumber(0, "usd")).toBe("$0.00");
+    expect(target.formatNumber(-0, "eur")).toBe("€0.00");
+    expect(target.formatNumber(-0.0, "usd")).toBe("$0.00");
+    expect(target.formatNumber(-0.00, "eur")).toBe("€0.00");
+    expect(target.formatNumber(-0.004, "usd")).toBe("$0.00");
+    expect(target.formatNumber(-3.8999999999995704e-7, "eur")).toBe("€0.00");
+    expect(target.formatNumber(0.004, "usd")).toBe("$0.00");
+});
